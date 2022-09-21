a65282a583035fb3473838c598a9707ab0b8b056
aktiver <developer@aktiver.io>
2022-09-21 20:36:10 +0000

    authorization

--- s3.py
+++
@@ -1,9 +0,0 @@
-import os, json
-
-def init_aws_creds():
-    creds = json.load("aws.json")
-    os.environ["AWS_ACCESS_KEY_ID"] = creds["accesskey"]
-    os.environ["AWS_SECRET_ACCESS_KEY"] = creds["secretkey"]
-    
-    
-
